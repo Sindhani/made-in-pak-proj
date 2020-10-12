668b2a4f0185e1b49f456d1c9ce3a346503e7658
waheed <abc@test.com>
2020-10-12 13:59:33 +0500

    initial commit 2

---
+++ resources/views/backend/admin/dashboard.blade.php
@@ -0,0 +1,19 @@
+@extends('layout.app')
+@section('contents')
+    <div class="col-lg-12 col-md-12">
+        <div class="card">
+            <div class="card-body">
+                <h4 class="card-title">Basic Card</h4>
+                <h6 class="card-subtitle text-muted">Basic Card With Header & Footer</h6>
+            </div>
+
+            <div class="card-body">
+
+            </div>
+            <div class="card-footer border-top-blue-grey border-top-lighten-5 text-muted">
+
+            </div>
+        </div>
+    </div>
+
+    @stop
---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('backend.admin.dashboard');
+});
+Route::get('/users', function () {
+    return view('backend.admin.dashboard');
+})->name('users');
+
+require_once "admin.php";
+require_once "front.php";
+require_once "backend.php";
