aa2a9416bdda3ade1372a4f256092f5ac3d0cfe3
punjtani <ghulammadina2016@gmail.com>
2020-10-12 16:10:22 +0500

    ghulam_ali

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('backend.admin.dashboard');
-});
+// Route::get('/', function () {
+//     return view('backend.admin.dashboard');
+// });
 Route::get('/users', function () {
     return view('backend.admin.dashboard');
 })->name('users');
