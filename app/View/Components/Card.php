21e3a2c4487d14d9e33f524fde461e3ae88bf595
waheed <abc@test.com>
2020-10-12 15:34:04 +0500

    dashboard scafolding is done

---
+++ app/View/Components/Card.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\View\Components;
+
+use Illuminate\View\Component;
+
+class Card extends Component
+{
+    public $customers = 50;
+    public $buyers = 100;
+
+    /**
+     * Create a new component instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     *
+     * @return \Illuminate\Contracts\View\View|string
+     */
+    public function render()
+    {
+        return view('components.card');
+    }
+}
