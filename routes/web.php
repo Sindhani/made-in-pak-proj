9f14531ec65d97ed59b6bd56e36e4b1f16edfb36
waheed <abc@test.com>
2020-10-13 15:24:35 +0500

    login module implemented

--- routes/web.php
+++ routes/web.php
@@ -1,29 +1,24 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
-
-//Route::get('register', function(){
-//    return view('auth.register');
-//})->name('register');
-//Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
-//    return view('dashboard');
-//})->name('dashboard');
-
-//Route::get('/',function(){return view('backend.admin.dashboard');})->name('admin.dashboard');
-//Route::get('/abc',function(){return view('backend.admin.dashboard');})->name('admin.sellers');
-//Route::get('/abcd',function(){ return view('backend.admin.seller.index'); })->name('admin.buyers');
-//require_once "front.php";
-//require_once "backend.php";
 use App\Http\Controllers\RolesController;
-Route::get('register', function(){
+
+Route::get('/', function(){
+    return view('welcome');
+});
+
+Route::get('register', function () {
     return view('auth.register');
 })->name('register');
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
+
+Route::get('/roles', [RolesController::class, 'role']);
+
+
 require_once "admin.php";
 require_once "front.php";
 require_once "backend.php";
-Route::get('/roles',[RolesController::class,'role']);
+
 
