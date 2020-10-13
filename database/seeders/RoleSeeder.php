77d1897bebe3c484254378814afe4d4b46dbb914
waheed <abc@test.com>
2020-10-13 13:42:25 +0500

    spatie/permission package is installed a seeder is also created

---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('roles')->insert([
+            'name' => 'admin',
+            'guard_name' => 'web'
+            ]);
+        DB::table('roles')->insert([
+            'name' => 'buyer',
+            'guard_name' => 'web'
+        ]);
+        DB::table('roles')->insert([
+            'name' => 'seller',
+            'guard_name' => 'web'
+        ]);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-<<<<<<< HEAD
+
 //Route::get('register', function(){
 //    return view('auth.register');
 //})->name('register');
@@ -14,27 +14,7 @@ use Illuminate\Support\Facades\Route;
 //Route::get('/abcd',function(){ return view('backend.admin.seller.index'); })->name('admin.buyers');
 //require_once "front.php";
 //require_once "backend.php";
-include_once "admin.php";
-=======
 use App\Http\Controllers\RolesController;
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-// Route::get('/', function () {
-//     return view('backend.admin.dashboard');
-// });
-Route::get('/users', function () {
-    return view('backend.admin.dashboard');
-})->name('users');
-
 Route::get('register', function(){
     return view('auth.register');
 })->name('register');
@@ -46,4 +26,4 @@ require_once "admin.php";
 require_once "front.php";
 require_once "backend.php";
 Route::get('/roles',[RolesController::class,'role']);
->>>>>>> ghulam_ali
+
